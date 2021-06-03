98886798d660d7309940c776c63e9ebdc7b44ac0
Smirkey <guillaume.bertrand59@gmail.com>
2021-06-03 17:58:39 +0200

    fix

---
+++ ftc_utils/scrapy_helpers/items.py
@@ -0,0 +1,100 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class FtcScrapyItem(scrapy.Item):
+    es_index = 'items'
+
+    id = scrapy.Field()
+    brand = scrapy.Field()
+    category = scrapy.Field()
+    city = scrapy.Field()
+    color1 = scrapy.Field()
+    color2 = scrapy.Field()
+    country = scrapy.Field()
+    created_at = scrapy.Field()
+    currency = scrapy.Field()
+    description = scrapy.Field()
+    photo_ids = scrapy.Field()
+    price = scrapy.Field()
+    size = scrapy.Field()
+    status = scrapy.Field()
+    title = scrapy.Field()
+    updated_at = scrapy.Field()
+    view_count = scrapy.Field()
+    url = scrapy.Field()
+    user_id = scrapy.Field()
+    platform = scrapy.Field()
+    sourced_at = scrapy.Field()
+
+
+class FtcScrapyUser(scrapy.Item):
+    es_index = 'users'
+
+    id = scrapy.Field()
+    login = scrapy.Field()
+    gender = scrapy.Field()
+    item_count = scrapy.Field()
+    forum_msg_count = scrapy.Field()
+    followers_count = scrapy.Field()
+    following_count = scrapy.Field()
+    photo_id = scrapy.Field()
+    created_at = scrapy.Field()
+    city = scrapy.Field()
+    country = scrapy.Field()
+    business_id = scrapy.Field()
+    total_items_count = scrapy.Field()
+    description = scrapy.Field()
+    updated_at = scrapy.Field()
+    url = scrapy.Field()
+    platform = scrapy.Field()
+    sourced_at = scrapy.Field()
+
+
+class FtcScrapyBrand(scrapy.Item):
+    es_index = 'brands'
+
+    id = scrapy.Field()
+    title = scrapy.Field()
+    favourite_count = scrapy.Field()
+    item_count = scrapy.Field()
+    is_luxury = scrapy.Field()
+    url = scrapy.Field()
+    platform = scrapy.Field()
+    sourced_at = scrapy.Field()
+
+
+class FtcScrapyAccounts(scrapy.Item):
+    es_index = 'accounts'
+
+    id = scrapy.Field()
+    name = scrapy.Field()
+    legal_code = scrapy.Field()
+    email = scrapy.Field()
+    phone_number = scrapy.Field()
+    legal_name = scrapy.Field()
+    nationality = scrapy.Field()
+    entity_type = scrapy.Field()
+    status = scrapy.Field()
+    country = scrapy.Field()
+    platform = scrapy.Field()
+    sourced_at = scrapy.Field()
+
+
+class FtcScrapyPhotos(scrapy.Item):
+    es_index = 'photos'
+
+    id = scrapy.Field()
+    image_no = scrapy.Field()
+    width = scrapy.Field()
+    height = scrapy.Field()
+    dominant_color = scrapy.Field()
+    dominant_color_opaque = scrapy.Field()
+    url = scrapy.Field()
+    is_main = scrapy.Field()
+    platform = scrapy.Field()
+    sourced_at = scrapy.Field()
---
+++ ftc_utils/scrapy_helpers/pipelines.py
@@ -0,0 +1,135 @@
+import pandas as pd
+import gc
+import pickle
+from ..data.exporter import Exporter
+from elasticsearch import Elasticsearch, helpers
+import datetime
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+from requests_aws4auth import AWS4Auth
+import boto3
+
+
+class S3Pipeline(object):
+    def __init__(self, bucket_name):
+        """Initialize.
+        Args:
+            bucket_name (str): Name of the bucket to store items in.
+            platform (str): Name of the platform that is used as an s3 key
+                prefix, i.e. 'thuisbezorgd' or 'ubereats'.
+        """
+        # Injected by self.open_spider
+        self.exporter = Exporter(mode="s3", aws_bucket=bucket_name)
+        self.results = {}
+        self.max_items_buffer = 50000
+        self.current_hold_items = 0
+
+    def save_results(self, spider):
+        print(f'Reached buffer size of {self.max_items_buffer}')
+        print("Exporting results to s3...")
+        self.results = {
+            x: pd.DataFrame(y).applymap(str) for x, y in self.results.items()
+        }
+        self.exporter.export_results(spider.sourced_pages, self.results)
+
+    def close_spider(self, spider):
+        self.save_results(spider)
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            bucket_name=crawler.settings.get("BUCKET_NAME"),
+        )
+
+    def process_item(self, item, spider):
+        self.current_hold_items += 1
+        if self.current_hold_items > self.max_items_buffer:
+            self.save_results(spider)
+            self.current_hold_items = 0
+            del self.results
+            self.results = {}
+            gc.collect()
+            print('Cleaned results from memory')
+
+        if spider.current_category not in self.results.keys():
+            self.results[spider.current_category] = []
+        self.results[spider.current_category].append(item)
+        return item
+
+
+class ElasticsearchPipeline(object):
+    """Pipeline saving locacally on ElasticSearch
+    """
+
+    def __init__(self, platform, bucket_name, max_items_buffer):
+        self.bucket_name = bucket_name
+        self.platform = platform
+        host = ('search-ftc-search-qwscz5vukur2jlufkud'
+                'lbiqhli.eu-west-3.es.amazonaws.com')
+        region = 'eu-west-3'  # e.g. us-west-1
+        service = 'es'
+        credentials = boto3.Session().get_credentials()
+        awsauth = AWS4Auth(credentials.access_key, credentials.secret_key,
+                           region, service, session_token=credentials.token)
+        self.es = Elasticsearch(
+            hosts=[{'host': host, 'port': 443}],
+            http_auth=awsauth,
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection
+        )
+        self.exporter = Exporter(mode="s3", aws_bucket=bucket_name)
+        self.results = {}
+        self.current_hold_items = 0
+        self.max_items_buffer = max_items_buffer
+
+    @classmethod
+    def from_crawler(cls, crawler):
+        return cls(
+            platform=crawler.settings.get("PLATFORM"),
+            bucket_name=crawler.settings.get("BUCKET_NAME"),
+            max_items_buffer=crawler.settings.get("MAX_ITEMS_BUFFER")
+        )
+
+    def get_upload_generator(self):
+        for index, rows in self.results.items():
+            for row in rows:
+                # id is platform + id to avoid duplicates
+                # inbetween platforms
+                yield {
+                    "_index": index,
+                    "_id": f"{self.platform}_{row['id']}",
+                    "_source": row
+                }
+
+    def save_results(self):
+        generator = self.get_upload_generator()
+        helpers.bulk(self.es, generator)
+        self.results = {}
+        self.current_hold_items = 0
+
+    def close_spider(self, spider):
+        sourced_pages_pickle = pickle.dumps(spider.sourced_pages)
+        sourced_pages_path = f'{self.exporter.today}/sourced_pages.pickle'
+        self.exporter.s3_client.put_object(
+            Body=sourced_pages_pickle,
+            Bucket=self.bucket_name,
+            Key=sourced_pages_path
+        )
+        self.save_results()
+
+    def process_item(self, item, spider):
+        index = item.es_index
+        item['platform'] = self.platform
+        item['sourced_at'] = datetime.datetime.now().strftime(
+            '%d/%m/%Y %H:%M'
+        )
+        if index in self.results.keys():
+            self.results[index].append(dict(item))
+        else:
+            self.results[index] = [dict(item)]
+        self.current_hold_items += 1
+        if self.current_hold_items > self.max_items_buffer:
+            print('Reached max buffer size')
+            print('Saving results')
+            self.save_results()
+        return item
