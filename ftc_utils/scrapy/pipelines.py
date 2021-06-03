89576f5ff0b99df290adbc0b9833ba77a967b929
Smirkey <guillaume.bertrand59@gmail.com>
2021-06-04 01:50:25 +0200

    fix endless upload loop

--- ftc_utils/scrapy/pipelines.py
+++ ftc_utils/scrapy/pipelines.py
@@ -128,4 +128,5 @@ class ElasticsearchPipeline(object):
             print('Reached max buffer size')
             print('Saving results')
             self.save_results()
+            self.current_hold_items = 0
         return item
