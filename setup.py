5af79cb19bf705807abb11ddfdda6b386f320bcc
Smirkey <guillaume.bertrand59@gmail.com>
2021-06-03 18:09:11 +0200

    fix

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup
 setup(
     name="ftc-utils",
     description="Crawlers and db utils",
-    version="0.0.2",
+    version="0.0.3",
     packages=[
         "ftc_utils",
         "ftc_utils.data",
-        "ftc_utils.scrapy_helpers",
-        "ftc_utils.selenium_helpers",
+        "ftc_utils.scrapy",
+        "ftc_utils.selenium",
     ],
     url="https://github.com/bear-trends/ftc-utils",
     author="Smirkey&Anko59",
