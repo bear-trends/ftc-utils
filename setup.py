283fb0122c57b507428bbbeb8bb9b8117512f714
Smirkey <guillaume.bertrand59@gmail.com>
2021-06-03 17:54:58 +0200

    fix setup

--- setup.py
+++ setup.py
@@ -5,10 +5,16 @@ setup(
     description="Crawlers and db utils",
     version="0.0.2",
     package_dir={"": "src"},
-    packages=["data", "scrapy_helpers", "selenium_helpers"],
+    packages=[
+        "ftc_utils",
+        "ftc_utils.data",
+        "ftc_utils.scrapy_helpers",
+        "ftc_utils.selenium_helpers",
+    ],
     url="https://github.com/bear-trends/ftc-utils",
     author="Smirkey&Anko59",
     author_email="mariedelahouce@gmx.fr",
     keywords=["scraping", "sourcing", "leboncoin", "vinted"],
     include_package_data=True,
+    install_requires=requirements,
 )
